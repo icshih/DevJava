601f42286076f61f76ef452fd39daec25f2f13f7
I-chun SHIH <icshih@mac.com>
2016-11-21 10:31:56 +0100

    create folders

---
+++ test/gaia/cu4/du439/hib/hibTest.java
@@ -0,0 +1,51 @@
+package gaia.cu4.du439.hib;
+
+import static org.junit.Assert.*;
+
+import javax.persistence.Persistence;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import gaia.cu1.tools.util.props.PropertyLoader;
+
+public class hibTest {
+
+	SessionFactory sessionFactory;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		PropertyLoader.load();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+				.configure()
+				.build();
+		sessionFactory = new MetadataSources(registry)
+				.buildMetadata()
+				.buildSessionFactory();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+	}
+
+}
